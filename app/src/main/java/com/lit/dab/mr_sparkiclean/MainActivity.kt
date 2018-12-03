9f820cbe9e25fac3c3d7a853091905ed5ec64a86
Michael Hering <michaelhering@Michaels-MacBook-Pro-2.local>
2018-12-02 20:35:52 -0700

    Blob detection

--- app/src/main/java/com/lit/dab/mr_sparkiclean/MainActivity.kt
+++ app/src/main/java/com/lit/dab/mr_sparkiclean/MainActivity.kt
@@ -88,25 +88,26 @@ class MainActivity : AppCompatActivity() {
             Log.d("B: ", blue.toString())
         }
         */
-
-        getCoordsByColor(95, parseColor("red"), image, "red")
+        val filteredImage = IntArray(image.height*image.width)
+        getCoordsByColor(95, parseColor("red"), image, "red", filteredImage)
     }
 
-    private fun getCoordsByColor(threshold: Int, color: Int, image: Bitmap, filterby: String) {
-        val pixels = IntArray(image.height * image.width)
-        val coloredPixels = IntArray(image.height * image.width)
-        image.getPixels(pixels, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight())
+    private fun getCoordsByColor(threshold: Int, color: Int, image: Bitmap, filterby: String, coloredPixels: IntArray) {
+        val width = image.width
+        val height = image.height
+        val pixels = IntArray(height * width)
+        image.getPixels(pixels, 0, image.width, 0, 0, image.width, image.getHeight())
         for(i in pixels.indices) {
             when(filterby) {
                 "red" ->
                 {
-                    if (abs(red(pixels[i]) - red(color)) > threshold) {
+                    if (abs(red(pixels[i]) - red(color)) < threshold) {
                         //Allow this pixel into our final image
                         coloredPixels[i] = pixels[i]
                     }
 
                 }
-                /*"green" ->
+                "green" ->
                 {
                     if (abs(green(color) - green(pixels[i])) < threshold) {
                         //Allow this pixel into our final image
@@ -126,45 +127,137 @@ class MainActivity : AppCompatActivity() {
                         //Allow this pixel into our final image
                         coloredPixels[i] = pixels[i]
                     }
-                }*/
+                }
             }
         }
-        val width = image.width
-        var minX = 0
-        var minY = 0
-        var maxX = image.width
-        var maxY = image.height
-        for (i in coloredPixels.indices) {
-            val x = i%width
-            val y = i/width
-            if (coloredPixels[i] != 0 && x < minX) {
-                minX = x
-            }
-            if (coloredPixels[i] != 0 && x > maxX) {
-                maxX = x
-            }
-            if (coloredPixels[i] != 0 && y < minY) {
-                minY = y
-            }
-            if (coloredPixels[i] != 0 && y > maxY) {
-                maxY = y
-            }
 
-        }
-        Log.d("Maxx: ", maxX.toString())
-        Log.d("Minx: ", minX.toString())
-        Log.d("Maxy: ", maxY.toString())
-        Log.d("Miny: ", minY.toString())
 
-        /*for (i in minX+1..maxX-1) {
-            for(j in minY+1..maxY-1) {
-                coloredPixels[i + j * width] = parseColor("blue")
+        //Run blob detection...
+
+        val blobs: ArrayList<IntArray> = ArrayList()
+
+        //Scan for non-white pixel
+        /*for (i in 0..(coloredPixels.size - 1)) {
+            if (coloredPixels[i] != 0) {
+                //Start recursion
+                val x = i % width
+                val y = i / width
+                currentBlob[i] = coloredPixels[i]
+                getBlob(x, y, width, coloredPixels, currentBlob)
+                blobs.add(currentBlob)
+                //Remove pixels in blob from image, reset currentBlob, and keep scanning
+                for (i in (0..(currentBlob.size-1)).withIndex()) {
+                    coloredPixels[i] = 0
+                    currentBlob[i] = 0
+                }
             }
         }*/
 
-        val coloredBitmap = createBitmap(coloredPixels, image.width, image.height, Bitmap.Config.ARGB_8888)
+        for(i in coloredPixels.indices) {
+            if (coloredPixels[i] != 0) {
+                val currentBlob = IntArray(width * height)
+                val x = i % width
+                val y = i / width
+                getBlob(x, y, width, coloredPixels, currentBlob)
+                blobs.add(currentBlob)
+                for(i in currentBlob.indices) {
+                    if(currentBlob[i] != 0) {
+                        coloredPixels[i] = 0
+                    }
+                }
+            }
+        }
+        val currentBlob = IntArray(width * height)
+        getBlob(0, 0, width, coloredPixels, currentBlob)
+        blobs.add(currentBlob)
+
+        //Rest is working
+
+        //Filter blobs
+
+        //Get coordinates per blob
+        for (blob in blobs) {
+            Log.d("Proccessing blob", "b")
+            var maxX = 0
+            var maxY = 0
+            var minX = image.width
+            var minY = image.height
+            for (i in blob.indices) {
+                val x = i % width
+                val y = i / width
+                if (blob[i] != 0 && x < minX) {
+                    minX = x
+                }
+                if (blob[i] != 0 && x > maxX) {
+                    maxX = x
+                }
+                if (blob[i] != 0 && y < minY) {
+                    minY = y
+                }
+                if (blob[i] != 0 && y > maxY) {
+                    maxY = y
+                }
+
+            }
+            Log.d("Maxx: ", maxX.toString())
+            Log.d("Minx: ", minX.toString())
+            Log.d("Maxy: ", maxY.toString())
+            Log.d("Miny: ", minY.toString())
+
+            for (i in (minX + 1)..(maxX - 1)) {
+                coloredPixels[i + minY * width] = parseColor("blue")
+                coloredPixels[i + maxY * width] = parseColor("blue")
+                for (j in (minY + 1)..(maxY - 1)) {
+                    coloredPixels[minX + j * width] = parseColor("blue")
+                    coloredPixels[maxX + j * width] = parseColor("blue")
+                }
+            }
+        }
+        val displayedImage = createBitmap(coloredPixels, image.width, image.height, Bitmap.Config.ARGB_8888)
         val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageBitmap(coloredBitmap)
+        imageView.setImageBitmap(displayedImage)
+    }
+
+    private fun getBlob(x: Int, y: Int, width: Int, pixels: IntArray, blob: IntArray) {
+        val idx = x + y*width
+        val idxLeft = idx-1
+        val idxRight = idx+1
+        val idxTop = idx + width
+        val idxBottom = idx - width
+        Log.d("X", x.toString())
+        Log.d("Y", y.toString())
+        Log.d(":", "---------")
+
+        //For all neighbors where the pixel is in bounds, the pixel is not already a part of the blob, the pixel should be added to the blob
+        //Ddd to Blob and call getBlob on neighbor
+
+        if (x-1 >= 0){
+            if(blob[idxLeft] == 0 && pixels[idxLeft] != 0) {
+                blob[idxLeft] = pixels[idxLeft]
+                getBlob(x - 1, y, width, pixels, blob)
+            }
+        }
+
+        if (x+1 <= width){
+            if(blob[idxRight] == 0 && pixels[idxRight] != 0) {
+                blob[idxRight] = pixels[idxRight]
+                getBlob(x + 1, y, width, pixels, blob)
+            }
+        }
+
+        if(y-1 >= 0){
+            if(blob[idxBottom] == 0 && pixels[idxBottom] != 0) {
+                blob[idxBottom] = pixels[idxBottom]
+                getBlob(x, y - 1, width, pixels, blob)
+            }
+        }
+
+        if(y+1 <= pixels.size/width - 1) {
+            if(blob[idxTop] == 0 && pixels[idxTop] != 0) {
+                blob[idxTop] = pixels[idxTop]
+                getBlob(x, y + 1, width, pixels, blob)
+            }
+        }
     }
 
 }
