81cbc37e27d81877d881ebef571ff50998dd94d3
Michael Hering <mihe1609@colorado.edu>
2018-12-05 21:05:44 -0700

    merged master

--- app/src/main/java/com/lit/dab/mr_sparkiclean/MainActivity.kt
+++ app/src/main/java/com/lit/dab/mr_sparkiclean/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.lit.dab.mr_sparkiclean
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.setTitle(null)
+        val sparkiLogo: Button = findViewById(R.id.sparkibutton)
+        sparkiLogo.setOnClickListener {
+            val intent = Intent (this, SelectDeviceActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
 }
